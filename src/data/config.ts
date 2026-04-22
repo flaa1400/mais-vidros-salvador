9cb562bd6d3220bb4d285822fef3e8c0a5b07f9d
flaa1400 <fla141989@gmail.com>
2026-04-22 17:46:31 -0300

    deploy: src\data\config.ts

---
+++ src/data/config.ts
@@ -0,0 +1,23 @@
+export const SITE_CONFIG = {
+  title: "Mais Vidros Salvador | Vidraçaria Especialista em Vidros e Espelhos",
+  description: "A melhor vidraçaria de Salvador. Especialistas em box para banheiro, espelhos sob medida, cortinas de vidro e manutenção de vidros.",
+  url: "https://maisvidrossalvador.com.br",
+  author: "Mais Vidros Salvador",
+  locale: "pt-BR",
+  nap: {
+    name: "Mais Vidros Salvador",
+    address: "Av. Cardeal Avelar Brandão Villela, 1647 - Mata Escura, Salvador - BA",
+    phone: "(11) 96429-9548",
+    whatsapp: "5511964299548",
+    email: "contato@maisvidrossalvador.com.br",
+    location: {
+      city: "Salvador",
+      state: "BA",
+      zip: ""
+    }
+  },
+  social: {
+    instagram: "https://instagram.com/maisvidrossalvador",
+    facebook: "https://facebook.com/maisvidrossalvador"
+  }
+};
