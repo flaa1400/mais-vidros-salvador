8225aa5021267430538dba6923698767c298c72e
flaa1400 <fla141989@gmail.com>
2026-04-22 17:46:30 -0300

    deploy: src\content\config.ts

---
+++ src/content/config.ts
@@ -0,0 +1,27 @@
+import { defineCollection, z } from 'astro:content';
+
+const blogCollection = defineCollection({
+  type: 'content',
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    pubDate: z.date(),
+    image: z.string().optional(),
+    author: z.string().default('Vidraçaria Salvador'),
+  }),
+});
+
+const servicesCollection = defineCollection({
+  type: 'content',
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    icon: z.string().optional(),
+    order: z.number().default(0),
+  }),
+});
+
+export const collections = {
+  'blog': blogCollection,
+  'servicos': servicesCollection,
+};
